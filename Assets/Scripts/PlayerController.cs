ae14fbf12d876eb09ea660ae758cc8efe84c5ac4
Jonah T <55107306+jmthomas6@users.noreply.github.com>
2019-11-06 16:42:03 -0500

    Player move tweaks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rb;
+    private Collider2D _col;
+
+    [SerializeField]
+    private float _acceleration, _speed, _slowLimit, _jumpVel;
+
+    private bool _grounded;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _col = GetComponent<Collider2D>();
+        _grounded = true; // MOVE TO TRIGGER
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 newVel = _rb.velocity;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            newVel += Vector2.left * _acceleration;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            newVel += Vector2.right * _acceleration;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded)
+        {
+            newVel += Vector2.up * _jumpVel;
+        }
+
+        if (newVel.x > _speed)
+        {
+            newVel.x = _speed;
+        }
+        if (newVel.x < -_speed)
+        {
+            newVel.x = -_speed;
+        }
+        if (newVel.x < _slowLimit && newVel.x > -_slowLimit)
+        {
+            newVel.x = 0;
+        }
+        _rb.velocity = newVel;
     }
 }
