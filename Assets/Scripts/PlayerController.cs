5e565d6f6cad30eb451e949c77ee82e87e232c41
Jonah T <jmthomas6@my.waketech.edu>
2019-11-06 18:53:31 -0500

    Ground tag and double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _acceleration, _speed, _slowLimit, _jumpVel;
 
-    private bool _grounded;
+    private bool _grounded, _doubleJump;
 
-    void Start()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<Collider2D>();
-        _grounded = true; // MOVE TO TRIGGER
     }
 
-    void Update()
+    private void Update()
     {
         Vector2 newVel = _rb.velocity;
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
@@ -30,9 +29,17 @@ public class PlayerController : MonoBehaviour
         {
             newVel += Vector2.right * _acceleration;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && _grounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            newVel += Vector2.up * _jumpVel;
+            if (_grounded)
+            {
+                newVel += Vector2.up * _jumpVel;
+            }
+            else if (_doubleJump)
+            {
+                newVel += Vector2.up * _jumpVel;
+                _doubleJump = false;
+            }
         }
 
         if (newVel.x > _speed)
@@ -48,5 +55,22 @@ public class PlayerController : MonoBehaviour
             newVel.x = 0;
         }
         _rb.velocity = newVel;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GroundTrigger"))
+        {
+            _grounded = true;
+            _doubleJump = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GroundTrigger"))
+        {
+            _grounded = false;
+        }
     }
 }
