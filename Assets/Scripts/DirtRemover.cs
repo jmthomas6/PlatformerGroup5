0958a92bf02048684205ab0413e8a8d2f4ce0229
mattprice32 <55508810+mattprice32@users.noreply.github.com>
2019-11-25 15:46:03 -0500

    rooms done. working on dirt

---
+++ Assets/Scripts/DirtRemover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtRemover : MonoBehaviour
+{
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            Debug.LogError("tagged wall");
+            Destroy(gameObject);
+        }
+    }
+}
