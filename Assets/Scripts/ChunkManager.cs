64aa7f94d80650781fb21b0aba0a673ab57378e0
Jonah T <55107306+jmthomas6@users.noreply.github.com>
2019-12-03 20:06:22 -0500

    more room spawn tests

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    public GameObject rightDoor, leftDoor, topDoor, bottomDoor;
+}
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -6,11 +6,20 @@ public class RoomManager : MonoBehaviour
 {
     private List<Vector2> _roomCoords, _blockedCoords, _tempCoords;
     private List<GameObject> _rooms;
-    private float _coordOffset, _iterations = 5f; // Change in inspector
-    private float _spawnRate = 0.5f;
+    [SerializeField]
+    private List<GameObject> _roomPrefabs;
+    [SerializeField]
+    private GameObject _startRoom, _gateRoom, _dirt;
+    [SerializeField]
+    private float _coordOffset, _roomCount, _spawnRate;
+    private int _gateIndex, _endIndex = 0;
 
     void Start()
     {
+        _roomCoords = new List<Vector2>();
+        _blockedCoords = new List<Vector2>();
+        _tempCoords = new List<Vector2>();
+        _rooms = new List<GameObject>();
         StartCoroutine(DetermineLayout());
     }
 
@@ -31,44 +40,147 @@ public class RoomManager : MonoBehaviour
         _blockedCoords.Add(new Vector2(0, 1));
         _blockedCoords.Add(new Vector2(0, -1));
 
-        //Iterate
-        foreach (Vector2 x in _roomCoords)
+        //Iterate first group
+        while (_roomCoords.Count < _roomCount)
         {
-            if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.up))
+            foreach (Vector2 x in _roomCoords)
             {
-                _tempCoords.Add(x + Vector2.up);
+                if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.up))
+                {
+                    _tempCoords.Add(x + Vector2.up);
+                }
+                if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.down))
+                {
+                    _tempCoords.Add(x + Vector2.down);
+                }
+                if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.left))
+                {
+                    _tempCoords.Add(x + Vector2.left);
+                }
+                if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.right))
+                {
+                    _tempCoords.Add(x + Vector2.right);
+                }
             }
-            if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.down))
+            foreach (Vector2 x in _tempCoords)
             {
-                _tempCoords.Add(x + Vector2.up);
+                _roomCoords.Add(x);
             }
-            if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.left))
+            _tempCoords.Clear();
+        }
+
+        // GATE ROOM
+        while (_gateIndex == 0)
+        {
+            foreach (Vector2 x in _roomCoords)
             {
-                _tempCoords.Add(x + Vector2.up);
+                int surroundingCount = 0;
+                if (!CheckRoomCoords(x + Vector2.up))
+                    surroundingCount++;
+                if (!CheckRoomCoords(x + Vector2.down))
+                    surroundingCount++;
+                if (surroundingCount < 2 && !CheckRoomCoords(x + Vector2.left))
+                    surroundingCount++;
+                if (surroundingCount < 2 && !CheckRoomCoords(x + Vector2.right))
+                    surroundingCount++;
+                if (surroundingCount < 2)
+                {
+                    _gateIndex = _roomCoords.IndexOf(x);
+                    // BLOCK OFF SURROUNDING CHUNKS
+                }
             }
-            if (Random.value < _spawnRate && CheckEmptyCoords(x + Vector2.right))
+            if (_gateIndex == 0)
             {
-                _tempCoords.Add(x + Vector2.up);
+                if (Random.value < 0.5f && CheckEmptyCoords(_roomCoords[_roomCoords.Count - 1] + Vector2.left))
+                {
+                    _tempCoords.Add(_roomCoords[_roomCoords.Count - 1] + Vector2.left);
+                    _roomCoords.Add(_tempCoords[0]);
+                    _tempCoords.Clear();
+                }
+                else if (CheckEmptyCoords(_roomCoords[_roomCoords.Count - 1] + Vector2.right))
+                {
+                    _tempCoords.Add(_roomCoords[_roomCoords.Count - 1] + Vector2.right);
+                    _roomCoords.Add(_tempCoords[0]);
+                    _tempCoords.Clear();
+                }
             }
         }
-        // Add temps to roomCoords
-        // Clear temps
-        // iterate 5 times
+
+        // END ROOM
+
+        StartCoroutine(SpawnRooms());
         yield return null;
     }
 
     IEnumerator SpawnRooms()
     {
-        // for each in roomcoords, spawn at roomcoords[x] * coordoffset
-        // add object to list of _rooms
-        // if roomCoords (up, down, left, right) is taken, despawn room door
+        foreach (Vector2 x in _roomCoords)
+        {
+            GameObject roomObj;
+            if (x == Vector2.zero)
+            {
+                roomObj = Instantiate(_startRoom);
+                roomObj.transform.position = x;
+            }
+            else if (_roomCoords.IndexOf(x) == _gateIndex)
+            {
+                roomObj = Instantiate(_gateRoom);
+                roomObj.transform.position = x;
+            }
+            else
+            {
+                int roomIndex = Random.Range(0, _roomPrefabs.Count);
+                roomObj = Instantiate(_roomPrefabs[roomIndex]);
+                roomObj.transform.position = x * _coordOffset;
+            }
+            _rooms.Add(roomObj); // Do I need this?
+
+            if (!CheckRoomCoords(x + Vector2.up))
+                roomObj.GetComponent<ChunkManager>().topDoor.SetActive(false);
+            if (!CheckRoomCoords(x + Vector2.down))
+                roomObj.GetComponent<ChunkManager>().bottomDoor.SetActive(false);
+            if (!CheckRoomCoords(x + Vector2.left))
+                roomObj.GetComponent<ChunkManager>().leftDoor.SetActive(false);
+            if (!CheckRoomCoords(x + Vector2.right))
+                roomObj.GetComponent<ChunkManager>().rightDoor.SetActive(false);
+        }
+
+        StartCoroutine(AddDirt());
         yield return null;
     }
 
     IEnumerator AddDirt()
     {
-        // find upper X and Y in roomcoords
-        // loop over ever coord combo, check if taken by room, spawn dirt obj
+        int minX = 0;
+        int maxX = 0;
+        int minY = 0;
+        int maxY = 0;
+        foreach (Vector2 x in _roomCoords)
+        {
+            if (x.x < minX)
+                minX = Mathf.RoundToInt(x.x);
+            if (x.x > maxX)
+                maxX = Mathf.RoundToInt(x.x);
+            if (x.y < minY)
+                minY = Mathf.RoundToInt(x.y);
+            if (x.y > maxY)
+                maxY = Mathf.RoundToInt(x.y);
+        }
+        minX--;
+        maxX++;
+        minY--;
+        maxY++;
+        for (int x = minX; x <= maxX; x++)
+        {
+            for (int y = minY; y <= maxY; y++)
+            {
+                if (CheckRoomCoords(new Vector2(x, y)))
+                {
+                    GameObject dirtObj = Instantiate(_dirt);
+                    dirtObj.transform.position = new Vector2(x * _coordOffset, y * _coordOffset);
+                }
+            }
+        }
         yield return null;
     }
 
