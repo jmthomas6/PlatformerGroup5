9280a0407f1a127d9f1e8edac94e88c8f3d9a20a
mattprice32 <55508810+mattprice32@users.noreply.github.com>
2019-12-09 15:41:55 -0500

    added enemy pooling

---
+++ Assets/Scripts/IPooledEnemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPooledEnemy 
+{
+    void OnObjectSpawn();
+
+    // https://www.youtube.com/watch?v=tdSmKaJvCoA water mark 15:00
+
+
+}
